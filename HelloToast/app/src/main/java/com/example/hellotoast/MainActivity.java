8baf7ee9c5cfc4a84bee670883c8356ddb3187a3
Mastan Vali P <mastanvali.apssdc@gmail.com>
2020-09-16 19:30:16 +0530

    upto sept16 added

---
+++ HelloToast/app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.hellotoast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    int count = 0;
+
+    TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tv = findViewById(R.id.textview);
+    }
+
+
+    public void displayMessage(View view) {
+        //Context ==>Application environment or class environment
+        //message
+        //time duration, LENGTH_LONG and LENGTH_SHORT
+        Toast.makeText(this,"Welcome to AAD Workshop",
+                Toast.LENGTH_LONG).show();
+    }
+
+    public void incrementCount(View view) {
+        count++;
+        tv.setText(""+count);
+    }
+}
